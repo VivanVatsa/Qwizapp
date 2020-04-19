0932cf122abd9d98d3ee4de934420ee3aa3e9cb8
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-04-19 23:44:49 +0530

    Update question.dart

--- lib/question.dart
+++ lib/question.dart
@@ -8,6 +8,6 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Text(questionText, style: TextStyle(fontSize: 30),);
   }
 }
